873125d5f88c92f4bab8db5e50fbb217a99fc381
Rajansharma05 <rajan.sharma_cs18@gla.ac.in>
2023-06-26 23:23:50 +0530

    Seventh Statement traversal a double linked list

--- src/assitedproject/DelecteFirstOccurrence.java
+++ src/assitedproject/DelecteFirstOccurrence.java
@@ -3,10 +3,12 @@ package assitedproject;
 class Node {
     int data;
     Node next;
+    Node previous;
 
     Node(int data) {
         this.data = data;
         this.next = null;
+        this.previous = previous; 
     }
 }
 
